df0cc080aaf2f7d023b1a8e398a60d8ba1b1b5b8
klabit87 <keith.labit@gmail.com>
2017-05-03 23:32:48 -0500

    Fix Magisk option

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -33,6 +33,10 @@ ifeq ($(WITH_SU),true)
 print_build_config_vars += \
   WITH_SU
 endif
+ifeq ($(WITH_MAGISK),true)
+print_build_config_vars += \
+  WITH_MAGISK
+endif
 ifeq ($(WITH_GMS),true)
 print_build_config_vars += \
   WITH_GMS
